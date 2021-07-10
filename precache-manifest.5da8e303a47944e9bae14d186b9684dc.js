2f3ff9f92afceee0480ec6af17bcb620d9521110
Kamil <kamil.bugno@onet.pl>
2021-07-10 15:59:23 +0200

    Updates

---
+++ precache-manifest.5da8e303a47944e9bae14d186b9684dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b29c2e1f5695b56c679b1662bc1520d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2ae1bfe1e269a3e6eabb",
+    "url": "./static/css/main.42cc8b22.chunk.css"
+  },
+  {
+    "revision": "85378aaca2384eef60b0",
+    "url": "./static/js/2.8c249889.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "./static/js/2.8c249889.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ae1bfe1e269a3e6eabb",
+    "url": "./static/js/main.82190c65.chunk.js"
+  },
+  {
+    "revision": "2c6b9a5e3a7e2540a97f",
+    "url": "./static/js/runtime-main.295b994c.js"
+  }
+]);
